546f6975bd319ed0d03e58c126310291adc8f772
Azim Ahmed <azim.ahmed7@gmail.com>
2022-02-24 20:27:31 -0500

    removed unused component

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,4 @@ export { Card } from './Card';
 export { Header } from './Header';
 export { IDForm } from './IDForm';
 export { Dropzone } from './Dropzone';
-export { DetailsModal } from './DetailsModal';
 export { UserProvider, useUserContext } from './UserContext';
